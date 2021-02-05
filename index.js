0214efc5a47b2600910f81d8b879a0a17c7cda89
Romario Gustave <romagustave10@gmail.com>
2021-02-05 21:45:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const k_muppet = {
 const animals_sounds=[
   'moo', 'neigh', 'bleat', 'oink', 'clucking'
 ]
+console.log(an)
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
 
